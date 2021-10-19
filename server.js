44ab299ed43affcf0364cd0617ae99032c40f3e9
Dave-Sama <davech1228@gmail.com>
2021-10-19 20:21:58 +0300

    new packages added

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+require('express-async-errors')
 
-const app = express();
+const app = express()
 
 app.get('/api/customers', cors(), (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
+	const customers = [
+		{ id: 1, firstName: 'John', lastName: 'Doe' },
+		{ id: 2, firstName: 'Brad', lastName: 'Traversy' },
+		{ id: 3, firstName: 'Mary', lastName: 'Swanson' },
+	]
 
-  res.json(customers);
-});
+	res.json(customers)
+})
 
-const port = 5000;
+const port = 5000
 
-app.listen(port, () => `Server running on port ${port}`);
+app.listen(port, () => `Server running on port ${port}`)
