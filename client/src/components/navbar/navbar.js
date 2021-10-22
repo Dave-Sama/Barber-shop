c26566d7ae2bbc0873cbd7ca81cb91c8ee1a4509
Dave-Sama <davech1228@gmail.com>
2021-10-23 00:12:49 +0300

    added background image and a bit of css

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,15 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, BrowserRouter as Router } from 'react-router-dom'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { FiSearch, FiShoppingCart } from 'react-icons/fi'
 
-const Navbar = () => {
+import '../../css/navbar.css'
+function Navbar() {
+	const [click, setClick] = useState(false)
+	const closeMobileMenu = () => setClick(false)
+	const handleClick = () => setClick(!click)
 	return (
 		<Router>
 			<nav className='navbar'>
 				<div className='navbar-container container'>
-					<Link to='/' className='navbar-log'>
-						<p>Dark Barber</p>
+					<Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+						<p className='code'>Dark Beard</p>
 					</Link>
+					<div className='menu-icon' onClick={handleClick}>
+						{click ? <FaTimes /> : <FaBars />}
+					</div>
 				</div>
+				<ul className={click ? 'nav-menu active' : 'nav-menu'}>
+					<li className='nav-item'>
+						<Link to='/' className='nav-links' onClick={closeMobileMenu}>
+							Home
+						</Link>
+					</li>
+					<li className='nav-item'>
+						<Link to='/skills' className='nav-links' onClick={closeMobileMenu}>
+							About Us
+						</Link>
+					</li>
+					<li className='nav-item'>
+						<Link to='/courses' className='nav-links' onClick={closeMobileMenu}>
+							Services
+						</Link>
+					</li>
+					<li className='nav-item'>
+						<Link to='/courses' className='nav-links' onClick={closeMobileMenu}>
+							Masters
+						</Link>
+					</li>
+					<li className='nav-item'>
+						<Link to='/courses' className='nav-links' onClick={closeMobileMenu}>
+							Prices
+						</Link>
+					</li>
+					<li className='nav-item'>
+						<Link to='/courses' className='nav-links' onClick={closeMobileMenu}>
+							Blog
+						</Link>
+					</li>
+					<li className='nav-item'>
+						<Link to='/courses' className='nav-links' onClick={closeMobileMenu}>
+							Booking
+						</Link>
+					</li>
+					<li className='nav-item'>
+						<Link to='/courses' className='nav-links' onClick={closeMobileMenu}>
+							Contacts
+						</Link>
+					</li>
+					<li className='icons'>
+						<Link to='/'>
+							<FiSearch className='fi-icon' />
+						</Link>
+					</li>
+					<li className='icons'>
+						<Link to='/'>
+							<FiShoppingCart className='fi-icon' />
+						</Link>
+					</li>
+				</ul>
 			</nav>
 		</Router>
 	)
